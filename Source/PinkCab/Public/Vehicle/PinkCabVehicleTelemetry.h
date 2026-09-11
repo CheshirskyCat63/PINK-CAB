660940d0489a05899790338ac91f91efb5c63254
CheCat <elesin.nikolai@gmail.com>
2026-09-12 02:36:47 +0300

    feat: add vendor-independent Tatra vehicle contracts

---
+++ Source/PinkCab/Public/Vehicle/PinkCabVehicleTelemetry.h
@@ -0,0 +1,12 @@
+#pragma once
+
+struct FPinkCabVehicleTelemetry
+{
+    float SpeedKmh = 0.0f;
+    float EngineRpm = 0.0f;
+    float NormalizedSteering = 0.0f;
+    float NormalizedThrottle = 0.0f;
+    float NormalizedBrake = 0.0f;
+    float NormalizedClutch = 0.0f;
+    float NormalizedHandbrake = 0.0f;
+};
