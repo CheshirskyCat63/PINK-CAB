07d6459552584cf4f4f3c7ba126b803622f794de
CheCat <elesin.nikolai@gmail.com>
2026-09-24 02:34:20 +0300

    build(CD-869): support editor-only road material generation

--- Source/PinkCabTests/PinkCabTests.Build.cs
+++ Source/PinkCabTests/PinkCabTests.Build.cs
@@ -10,7 +10,7 @@ public class PinkCabTests : ModuleRules
             "Core", "CoreUObject", "ApplicationCore", "Engine", "InputCore", "UnrealEd", "PinkCab",
             "ChaosVehicles", "PhysicsCore",
             "PinkCabCore", "PinkCabInteraction", "PinkCabVehicle", "PinkCabEconomy", "PinkCabWorld", "PinkCabTraffic", "PinkCabTaxi", "PinkCabPersistence",
-            "MetaRoad", "MetaRoadEditor", "StructUtils", "AssetRegistry"
+            "MetaRoad", "MetaRoadEditor", "StructUtils", "AssetRegistry", "MaterialEditor"
         });
     }
 }
